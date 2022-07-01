9a07f43226bc0355d274c1f0e9ad09494e11549c
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-01 16:42:37 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - assign a random number
+ * Return: always (0) success
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+	return (0);
+}
