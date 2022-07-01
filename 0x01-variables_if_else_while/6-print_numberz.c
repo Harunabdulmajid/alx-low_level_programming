3518673fe9ce43fafdd61298a06469185e19579c
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-01 16:22:07 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * prints all single digit numbers of base 10 starting from
+ * Return: always (0) success
+ */
+int main(void)
+{
+	int ch;
+	for (ch = 48; ch <= 57; ch++)
+}
+
+putchar(ch);
+}
+
+putchar(10);/* This is an ascii code for new line */
+
+return (0);
+}
