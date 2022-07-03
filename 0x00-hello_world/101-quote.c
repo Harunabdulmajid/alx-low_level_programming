c9c6ad647b99e2b7d0583cc5cfa2342df231390c
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-03 01:50:03 -0700

    1-compiler

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <sys/syscall.h>
+/**
+ * main - prints 
+ * Return: Always(1) success
+ */
+int main(void)
+{
+	const char msg[] = "and that piece of art is useful" - Dora korpar, 2015-10-19,\n"
+	syscall(SYS_write, 1, msg, sizeof(msg))
+	return (1);
+}
