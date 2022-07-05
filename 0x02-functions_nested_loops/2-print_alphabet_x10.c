7ccfb814633c82f9efdcc616a4e1c53909644aa1
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-05 14:14:43 -0700

    hot fix

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main"
+/**
+ * main - prints alphabet 10x
+ *
+ * Return:
+ */
+void print_alphabet_x10(void)
+{
+	int m;
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		_putchar(m);
+	}
+	_putchar('\n');
+}
