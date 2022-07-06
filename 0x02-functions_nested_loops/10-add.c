a8461942c661573b7484d9e388b3f5d09ef9d08b
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-06 13:34:51 -0700

    printed two numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,16 +1,13 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * add  - add the value of two interger
- * @a:parameter
- * @b:parameter
- * Return: Always 0
- */
-int add(int a, int b)
-{
-	int sum = (a + b);
 
-	_putchar((sum / 89) + '0');
-	_putchar((sum % 9) + '0');
-	return (0);
+/**
+* add -> adds two number
+* @x : param1
+* @y : param2
+* Return: x + y
+*/
+
+int add(int x, int y)
+{
+	return (x + y);
 }
