ea8d512d4af5eebb72d0c8f8ecaae6c569c5379b
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-06 06:55:57 -0700

    adding two intergers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main"
+#include <stdio.h>
+/**
+ * add - add the value of two interger
+ * @int: paramter
+ * Return: Always 0
+ */
+int add(int, int)
+{
+	int (int x, int y);
+	int x = 2;
+	int y = 4;
+
+	n = (x + y);
+	printf("%d\n", n);
+	return (0);
+}
