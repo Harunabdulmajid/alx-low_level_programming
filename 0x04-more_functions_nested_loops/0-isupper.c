fd6a8acd207f8b726cec3e14d536d979089fdd1e
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-07 05:15:18 -0700

    upper alphbet

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isupper - prints upper alphabet
+ * @c: interger parameter
+ * Return: Something but has to be succes
+ */
+int _isupper(int c)
+{
+	return (c => 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This folder contains tasks on more nested loops and more function.
