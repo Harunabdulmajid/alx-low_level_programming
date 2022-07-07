d8e05bc5706aa505fe75222069ed94d3597d05a7
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-07 06:21:47 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ * @x: parameter
+ * Return: Success
+ */
+void print_numbers(void)
+{
+	for (x = 0, x < 10, x++)
+		_putchar(x + '0');
+	_putchar('\n');
+}
