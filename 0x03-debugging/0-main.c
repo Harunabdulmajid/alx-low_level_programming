041b584aba685d54a9d4fbbca03cec7cd79171c3
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-07 15:56:35 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,25 +1,17 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
+#include "main.h"
 /**
- *main -> assign a random number to the variable n each time it is executed
- *and print the last digit of the number stored in the variable n
- *Return: Always 0 (Success)
+ * main - testing to detect bug
+ * Return: always 0
  */
+
 int main(void)
 {
-int n;
+	int i = 0;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-	printf("%d is positive\n", n);
-if (n == 0)
-	printf("%d is zero\n", n);
-if (n < 0)
-{
-	printf("%d is negative\n", n);
-}
-return (0);
+	positive_or_negative(i);
+
+	return (0);
 }
