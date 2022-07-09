52413e243295cab2a19b3324d49a420fc1726e55
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-09 13:49:48 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-#include <sys/syscall.h>
+#include <unistd.h>
+
 /**
- * main - prints 
- * Return: Always(1) success
- */
+* main - prints exactly "and that piece of art is useful"
+* - Dora Korpar, 2015-10-19",
+* followed by a new line, to the standard error.
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	const char msg[] = "and that piece of art is useful" - Dora korpar, 2015-10-19,\n"
-	syscall(SYS_write, 1, msg, sizeof(msg))
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
