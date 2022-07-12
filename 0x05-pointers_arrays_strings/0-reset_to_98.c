2c38c8a2d7a8f5e1328a78cea668729cf1d86ec0
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-12 05:58:32 -0700

    pointers and arrays

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,15 @@
 /**
  * reset_to_98 - points a parameter and update the value
  * @n: interger param
- * Return: Always 0 
+ * Return: Always 0
  */
 void reset_to_98(int *n)
 {
+	int n;
+
+	n = 98;
+	_putchar("n=%d\n", n);
+	reset_to_98(&n);
+	_putchar("n=%d\n", n);
+	return (0);
+}
