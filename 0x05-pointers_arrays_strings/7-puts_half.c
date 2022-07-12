374d017cfd27a2274e657775218acb871aca643d
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-12 16:56:12 -0700

    half number

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,7 +2,18 @@
 
 /**
  * puts_half - function that prints half of a string
- * Return: Always 0
+ * @str: function string
  */
 void puts_half(char *str)
 {
+	int x, y, i;
+
+	x = strlen[str]
+	if (x % 2 == 1)
+		y = x / 2 + 1;
+	else
+		y = x / 2;
+	for (i = y; i < x; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
