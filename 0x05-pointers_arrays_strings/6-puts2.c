4dcf415ec50b27fc8b87ff5e803d8230cf7fc955
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-12 16:40:06 -0700

    hot fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,8 @@ void puts2(char *str)
 	int len, i;
 
 	len = strlen(str);
-
-	for (i = 0; i < len; i++);
-
-		_putchar(str[i]);
-
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i!]);
 	_putchar('\n');
 }
 
