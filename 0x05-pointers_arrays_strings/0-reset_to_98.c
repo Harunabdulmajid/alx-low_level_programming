358c38f5b6a275f99bf532b1bee99b3469083c84
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-12 06:02:23 -0700

    pointers and arrays

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -10,9 +10,9 @@ void reset_to_98(int *n)
 {
 	int n;
 
-	n = 98;
-	_putchar("n=%d\n", n);
+	n = 402;
+	printf("n=%d\n", n);
 	reset_to_98(&n);
-	_putchar("n=%d\n", n);
+	printf("n=%d\n", n);
 	return (0);
 }
