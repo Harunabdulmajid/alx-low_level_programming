fc0c58fb31d5243c1b17aee2dca1108c25fbeb28
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-12 13:57:01 -0700

    hot fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,6 +4,7 @@
  * print_rev - funtion that prints a string
  * @s: string to be printed
  */
+
 void print_rev(char *s)
 {
 	int i, n;
@@ -14,8 +15,7 @@ void print_rev(char *s)
 
 	for (i = n - 1; i >= 0; i--)
 	{
-		_putchar(s[i]);
-	}
 	_putchar('\n');
+	}
 }
 
