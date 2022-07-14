3f0d59e89e4ccc64d9120a9d1c949f23e53521e9
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-14 15:13:27 -0700

    creating files for C-pointers,arrays and strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,13 +7,12 @@
  */
 char *string_toupper(char *x)
 {
-	int i = 0;
+	int i;
 
-	while (x[i])
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (x[i] >= 97 && x[i] <= 122)
-			x[i] = x[i] = 32;
-		i++;
+		if (str[i] <= 'z' && str[i] >= 'a')
+			str[i] -= 32;
 	}
-	return (x);
+	return (str);
 }
