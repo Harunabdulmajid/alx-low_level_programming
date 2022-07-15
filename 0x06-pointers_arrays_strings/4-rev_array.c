70c0eef1b42134c8b303f68d4d378126686e4d3b
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-14 18:11:00 -0700

    hot fix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,14 +6,17 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i, j, tmp;
+	int *p, i, aux, k;
 
-	j = j - 1;
-
-	for (i = 0; i < n / 2; i++)
+	p = a;
+	for (i = 0; i < n; i++)
+		p++;
+	for (k = 0; k < i / 2; k++)
 	{
-		tmp = a[i];
-		a[i] = a[j];
-		a[j--] = tmp;
+		aux = a[k];
+		a[k] = *p;
+		*p = aux;
+		p--;
 	}
 }
+
