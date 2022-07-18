5ddaa1da6a932f96d0a0dbb379dab6bf6316972a
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-18 09:00:34 -0700

    hot fix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,13 +3,13 @@
 
 /**
  * _memcpy - copy memory area
- * @dest - address of memory
+ * @dest: - address of memory
  * @src: size of memory
  * @n: interger
  * Return: dest
  */
- 
 char *_memcpy(char *dest, char *src, unsigned int n)
+{
 	unsigned int a;
 
 	for (a = 0; a < n; a++)
