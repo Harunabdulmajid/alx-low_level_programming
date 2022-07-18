8ce0e4088ea10b278732edd433c946d6f8b96faf
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-18 12:54:06 -0700

    hot fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,20 +9,20 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int a, b;
-	int x = 0;
+	int c = 0;
 
 	for (a = 0; s[a] != '\0'; a++)
 	{
 		if (s[a] != 32)
 		{
-			for (b = 0; accept[b] != '\0'; b++)
+			for (b == 0; accept[b] != '\0'; b++)
 			{
 				if (s[a] == accept[b])
-					x++;
+					c++;
 			}
 		}
 		else
-			return(x);
+			return (c);
 	}
-		return (x);
+	return (c);
 }
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,12 +9,18 @@ void print_chessboard(char (*a)[8])
 {
 	int c, b;
 
-	for (c = 0; c < 8; c++)
+	for (c = 0; a < 8; c++)
 	{
 		for (b = 0; b < 8; b++)
 		{
-			_putchar(*(*(c + a) + b));
+			if (b == 7)
+			{
+				_putchar(a[c][b]);
+
+				_putchar('\n');
+			}
+			else
+				_putchar(a[c][b]);
 		}
-		_putchar('\n');
 	}
 }
