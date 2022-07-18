c2b3a1f62258694468d56cb1383f9089cb132e39
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-18 15:13:49 -0700

    hot fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,7 +15,7 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		if (s[a] != 32)
 		{
-			for (b == 0; accept[b] != '\0'; b++)
+			for (b = 0; accept[b] != '\0'; b++)
 			{
 				if (s[a] == accept[b])
 					c++;
