97332e13ec2d54e96d05c2f6229116dcff57622b
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-18 14:47:59 -0700

    print_chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,18 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_chessboard - prints the chessboard.
  * @a: row of array
  * @size: param1
  * Return: nothing
  */
-void print_chessboard(char (*a)[8])
+void print_chessboard(int *a, int size)
 {
-	int c, sum1 = 0; sum2 = 0;
+	int c, sum1 = 0, sum2 = 0;
 
 	for (c = 0; c < size; c++)
 	{
 		sum1 += a[(size + 1) * c];
-		sum2 += a[(size -1) * (c + 1)];
+		sum2 += a[(size - 1) * (c + 1)];
 	}
 	{
 		printf("%d, %d\n" sum1, sum2);
