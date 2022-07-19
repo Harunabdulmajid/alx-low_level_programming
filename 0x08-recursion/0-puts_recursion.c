45eba888925a9157ded3b4b8e6d802e92e486f0c
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-19 14:17:10 -0700

    print a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts_recursion - print a string
+ * @s: string to be printed
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	char s[] = "This is a string";
+
+	_puts_recurusion(s);
+	_putchar('\n');
+}
