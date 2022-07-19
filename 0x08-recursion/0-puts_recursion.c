72093093ddb0d50739d0b6dacd750580a83b4d80
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-19 14:29:38 -0700

    print a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,10 +6,11 @@
  */
 void _puts_recursion(char *s)
 {
-	char s[] = ("First solve the problem. Then, write the code");
-
-	_puts_recurusion(s);
-	_putchar("%c", s);
-	_putchar('\n');
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
-
