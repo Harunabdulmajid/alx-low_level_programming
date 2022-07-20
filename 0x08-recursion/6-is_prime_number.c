960a7c66a2a60d41ae94f7a94c26f39996f78812
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-20 15:59:05 -0700

    compares two strings

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,27 +1,7 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * prime - searches for a prime number
  * @a: number to be searched
  * @b: numbers to be examined
- * Return: a value that is prime or not
- */
-int prime(int a, int b)
-{
-	if (a <= 1 || a % b == 0)
-		return (0);
-	else if (a == b)
-		return (1);
-	else if (a > b)
-		prime(a, b + 1);
-	return (1);
-}
-
-/**
- * is_prime_number - determine if its an interger or not
- * @n: number to check
- * Return: 0 if the number is a prime, 1 if its not
- */
-int is_prime_number(int n)
-{
-	return (prime(n, 2));
-}
+ * Return: a value that 
