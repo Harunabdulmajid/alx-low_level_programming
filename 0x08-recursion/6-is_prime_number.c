3431ec07d23223979da1430c9d1fd96de4d2d29b
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-21 01:43:58 -0700

    header file

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,4 +4,29 @@
  * prime - searches for a prime number
  * @a: number to be searched
  * @b: numbers to be examined
- * Return: a value that 
+ * Return: a value that
+ */
+int primeSearcher(int a, int b)
+{
+	if (a <= 1 || a % b == 0)
+		return (0);
+	else if (a == b)
+		return (1);
+	else if (a > b)
+		primeSearcher(a, b + 1);
+
+	return (1);
+}
+
+/**
+ * is_prime_number - determines prime number
+ *
+ * @n: the number to check
+ *
+ * Return: 0 is its a prime number, 1 if its not
+ */
+
+int is_prime_number(int n)
+{
+	return (primeSearcher(n, 2));
+}
