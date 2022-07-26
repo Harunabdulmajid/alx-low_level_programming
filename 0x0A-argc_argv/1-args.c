0f591c54867582092a8daa2f9907e4bebf83f606
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-26 09:36:40 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,13 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int c;
-
-	void(argv);
-
-	for (c = 0; c <= argc; c++)
-	{}
-	printf("%d\n", c - 2);
-
+	printf("%d\n", argc -1);
+	argv++;
 	return (0);
 }
