71d0768bd6b62ac509046cbe0198606e6695526a
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-07-27 12:03:14 -0700

    4-free_grid.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,23 +10,3 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int size1 = 0; size2 = 0, i = 0, j = 0;
-	char *new;
-
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	while (s1[size1] != '\0'
-	{
-		size++;
-	}
-	while (s1[size2] != '\0'
-	{
-		size++;
-	}
-	new = malloc((sizeof(char) * size1)
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+/**
+ * free_grid - allocate grid
+ * @grid: pointer to 2D arrays
+ * @height: array
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	height--;
+
+	while (heighy >= 0)
+	{
+		free(*(grid + height));
+		height--;
+	}
+	free(grid);
+}
