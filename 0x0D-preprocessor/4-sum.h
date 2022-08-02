6aa4bae70cdec03637b2fae34e6c0834a6fa7a57
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-08-02 02:41:08 -0700

    changes made

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,12 +1,5 @@
-#include <stdio.h>
-#define SUM(x,y) x + y
-/**
- 
-* main - computes the name of two numbers
- * Return: 0 success
- */
-int main(void)
-{
-	printf("%d", SUM(2+2));
-	return (0);
-}
+#ifndef SUM_H
+#define SUM_H
+
+#define SUM(x,y) ((x) + (y))
+#endif
