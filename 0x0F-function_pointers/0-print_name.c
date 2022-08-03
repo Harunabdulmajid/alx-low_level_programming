1857ca3d77749c2ef7d5d8e992decff326791307
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-08-03 10:34:28 -0700

    task 0, 1 and 2 solved

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name.
+ * @name: name to be printed
+ * @f: pointer to print upper or non upp
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f = NULL)
+		return;
+	f(name);
+}
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include <stdlib>
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - print array element
+ * @array: array
+ * @size: size
+ * @action: pointer to print in regular or hex
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL)
+		return;
+	if (action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
