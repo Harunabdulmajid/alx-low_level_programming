fcd4671c0ad81c5bb9a183732abd0dfa582e2f73
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-08-11 17:03:07 -0700

    header file edited

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,19 @@
 #ifndef LIST_H
 #define LIST_H
+#include <unistd.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: length of string
+ * @next: point to next pointer
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_s;
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
