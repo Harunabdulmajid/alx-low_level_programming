841d6e3df2c57ce81f9b0447a28e534f013f7413
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-08-11 17:56:09 -0700

    header file created

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,25 +1,24 @@
 #ifndef LIST_H
 #define LIST_H
+#include <unistd.h>
 
-#include <stdio.h>
-#include <stdlib.h>
 /**
- * struct list_s - singly linked list
+ * struct list_s - singly link list
  * @str: string
  * @len: length of string
- * @next: point to next pointer
+ * @next: point to the next node
  */
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-} list_s;
+} list_t;
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
