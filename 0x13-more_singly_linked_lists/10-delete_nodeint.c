4f7d843a610ccfd6b55f4ec3fff0c9b663974da8
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-08-16 09:51:25 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,3 +10,26 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *new, *copy = *head;
+	unsigned int node;
+
+	if (copy == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(copy);
+		return (1);
+	}
+
+	for (node = 0; node < (index - 1); node++)
+	{
+		if (copy->next == NULL)
+			return (-1);
+		copy = copy->next;
+	}
+	temp = copy->next;
+	copy->next = temp->next;
+	free(temp);
+	return (1);
+}
