35ee9d823e578ff29203495aec59e84e6825cc1b
harunabdulmajid <harunaabdulmajid7@gmail.com>
2022-08-26 03:34:53 -0700

    hot fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file and prints it.
  * @filename: file to read
  * @letters: no. of letters to read and print
  *
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,5 +10,6 @@ int append_text_to_file(const char *filename, char *text_content);
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stddef.h>
 
 #endif
