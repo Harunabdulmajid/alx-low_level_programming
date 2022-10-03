1931fc3ce6d88f05e9c8a4591ded5dc94fe4b796
harunabdulmajid <harunaabdulmajid@gmail.com>
2022-10-03 16:44:14 -0700

    fix

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-echo "Creating Dynamic Library..."
-gcc -g -fPIC -Wall -Wextra -Werror -pedantic  -std=gnu89 *.c -shared -o liball.so
-ls liball.so
-echo "Complete"
+gcc *.o -shared -o liball.so
